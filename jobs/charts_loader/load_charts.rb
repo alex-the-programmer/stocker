7cb2d6ba067dc436d5d9f0147ef8fe13d2d26446
Alex <AlexTheProgrammerMail@gmail.com>
2019-01-05 20:58:56 -0800

    cheap scope check added to the chart loader

--- jobs/charts_loader/load_charts.rb
+++ jobs/charts_loader/load_charts.rb
@@ -3,7 +3,7 @@ module ChartsLoader
     include Sidekiq::Worker
 
     def perform
-      Company.in_batches do |companies_batch|
+      Company.cheap.in_batches do |companies_batch|
         companies_batch.ids.each do |company_id|
           LoadChart.perform_async company_id
         end
