03bafe3441a5097bba56a7370d385dc07965db2d
Alex <AlexTheProgrammerMail@gmail.com>
2019-01-21 00:02:21 -0800

    more test stubs written

--- spec/jobs/learning_data_preparer/prepare_learning_record_spec.rb
+++ spec/jobs/learning_data_preparer/prepare_learning_record_spec.rb
@@ -41,19 +41,36 @@ describe LearningDataPreparer::PrepareLearningRecord do
   end
 
   describe '#last_30_marget_days' do
+    it 'returns last 30 days including the given one that have charts' do
 
+    end
+    describe 'when there is not enough dates' do
+      # TBD
+    end
   end
 
   describe '#last_20_weeks' do
+    it 'retuns last 20 dates that are a week apart from one another and where holidays are replaced with last market days' do
 
+    end
+    describe 'when there is not enough dates' do
+      # TBD
+    end
   end
 
   describe '#last_24_months' do
+    it 'returns last 24 dates that are a month apart from one anotehr and where holidays are replaced with last market days' do
 
+    end
+    describe 'when there is not enough dates' do
+      # TBD
+    end
   end
 
   describe '#days_that_have_charts' do
+    it 'returns only days that have charts for them' do
 
+    end
   end
 
   describe '#find_last_market_date_for' do
@@ -88,11 +105,41 @@ describe LearningDataPreparer::PrepareLearningRecord do
   end
 
   describe '#find_next_market_date_for' do
+    describe 'when next day is a weekday' do
+      describe 'when there is chart for that day' do
+        it 'returns a chart for that day' do
 
+        end
+      end
+      describe 'when there is no charts for that day' do
+        describe 'when there is no more market days for the symbol' do
+         it 'raises an exception'
+
+        end
+        describe 'when there are more days for the symbol' do
+          it 'returns a chart for the next market day day' do
+
+          end
+        end
+      end
+    end
+    describe 'when the next day is a weekend' do
+      it 'returns next market day chart' do
+
+      end
+    end
   end
 
   describe '#replace_holidays_with_last_market_days' do
+    it 'returns the days themselves for normal market days' do
 
+    end
+    it 'returns last market days for holidays and weekends' do
+
+    end
+    describe 'when there is no later market days for the symbol' do
+      # TODO TBD what to do here
+    end
   end
 
   describe '#features_for_interval' do
